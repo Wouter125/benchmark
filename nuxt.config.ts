5c1143fd31a0c1bb84a2560aef4878f5870f458c
Wouter <wouter@thisisnota.studio>
2023-08-03 13:19:19 +0200

    fix: remove cdn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   nitro: {
     preset: 'netlify',
     serveStatic: true
   },
-  app: {
-    cdnURL: "https://dtpu9zq1caghj.cloudfront.net"
-  }
-  
 })
